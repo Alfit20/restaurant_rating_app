2ec8531d183551a1c29dab1d458fd06c7cdd4cd2
BashirovAlfit <alfit.bashirov@gmail.com>
2022-05-31 13:08:16 +0600

    Added configurations

---
+++ src/main/java/com/example/app_restaurant/config/FileStoreConfiguration.java
@@ -0,0 +1,12 @@
+package com.example.app_restaurant.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "file-store")
+public class FileStoreConfiguration {
+    private String path;
+}
---
+++ src/main/java/com/example/app_restaurant/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.example.app_restaurant.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@AllArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final DataSource dataSource;
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage("/login")
+                .failureUrl("/login?error=true");
+
+        http.logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .clearAuthentication(true)
+                .invalidateHttpSession(true);
+
+        // Только после авторизации
+        http.authorizeRequests()
+                .antMatchers("/add/**", "/review/**")
+                .authenticated();
+
+        http.authorizeRequests()
+                .anyRequest()
+                .permitAll();
+        http.csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        String fetchUsersQuery = "SELECT email, password, enabled FROM customers WHERE email = ?";
+        String fetchRolesQuery = "SELECT email, role FROM customers WHERE email = ?";
+        auth.jdbcAuthentication()
+                .usersByUsernameQuery(fetchUsersQuery)
+                .authoritiesByUsernameQuery(fetchRolesQuery)
+                .dataSource(dataSource);
+    }
+}
